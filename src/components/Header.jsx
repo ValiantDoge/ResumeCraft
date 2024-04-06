deebecf85fb34e061edcd7184378514d0f0901a0
ValiantDoge <agnelofernandes475@gmail.com>
2024-04-06 16:06:44 +0530

    Started ResumeCraft with React+Django

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import resumeCraftLogo from '../assets/resumeCraftlogo.png'
+export default function Header() {
+    return <header class="text-gray-600 body-font">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+            <a class="mr-5 hover:text-gray-900">First Link</a>
+            <a class="mr-5 hover:text-gray-900">Second Link</a>
+            <a class="mr-5 hover:text-gray-900">Third Link</a>
+            <a class="hover:text-gray-900">Fourth Link</a>
+        </nav>
+        <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
+            <img src={resumeCraftLogo} className='w-14 h-14 text-white rounded-full' alt="resumeCraftLogo" />
+            <span class="ml-3 text-xl">ResumeCraft</span>
+        </a>
+        <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+            <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+            </button>
+        </div>
+        </div>
+  </header>
+}
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function HelloWorld() {
+  const [msg, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/hello-world/')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello, World!</h1>
+      <p>{msg}</p>
+    </div>
+  );
+}
+
+export default HelloWorld;

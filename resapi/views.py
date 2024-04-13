a5a046385c6ca340f014ed0cbba891ae566c28ed
ValiantDoge <agnelofernandes475@gmail.com>
2024-04-14 01:02:11 +0530

    Integrating Flowables

--- resapi/views.py
+++ resapi/views.py
@@ -4,13 +4,13 @@ from django.conf import settings
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase.pdfmetrics import registerFont, registerFontFamily
 from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.lib.pagesizes import letter
+from reportlab.lib.pagesizes import A4
 from reportlab.lib import colors
 from reportlab.graphics.shapes import *
 from PIL import Image as PImage
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.lib.units import mm
-from reportlab.platypus import Paragraph
+from reportlab.lib.units import mm,inch
+from reportlab.platypus import Paragraph, Table, TableStyle
 
 
 from rest_framework.decorators import api_view
@@ -25,16 +25,29 @@ registerFont(TTFont('LatoBlack', os.path.join(settings.BASE_DIR, 'public', 'font
 registerFont(TTFont('Lora',  os.path.join(settings.BASE_DIR, 'public','fonts', 'Lora','Lora-Regular.ttf') ))
 registerFont(TTFont('LoraItalic',  os.path.join(settings.BASE_DIR, 'public','fonts', 'Lora','Lora-Italic.ttf') ))
 
+registerFont(TTFont('FontAwesome', os.path.join(settings.BASE_DIR, 'public','fonts', 'FontAwesome.ttf')))
+registerFontFamily('Icon', normal='FontAwesome')
+
+
 registerFontFamily('Lora', normal='Lora', bold='LoraItalic')
+registerFontFamily('Lato', normal='Lato', bold='LatoBlack')
 style = getSampleStyleSheet()
 style.add(ParagraphStyle(name='Content',
                     fontFamily='Lora',
                     fontSize=14,
                     leading=20,
-                    wordWrap='LTR',
-                    spaceAfter=0,
-                    spaceBefore=0,
                     ))
+
+style.add(ParagraphStyle(name='IconHere', 
+                          spaceBefore=1,
+                          leading=20,
+                          fontFamily ='Icon', 
+                          fontSize=15))
+
+style.add(ParagraphStyle(name='SectionTitle',
+                         fontFamily='Lato',
+
+                          ))
 
 @api_view(['GET', 'POST'])
 def createResume(request):
@@ -51,12 +64,12 @@ def createResume(request):
 
         #Creating the Canvas
         buf = io.BytesIO()
-        c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+        c = canvas.Canvas(buf, pagesize=A4)
         
-        canvas_width, canvas_height = letter
+        canvas_width, canvas_height = A4
         #Draw the style rectangles
         d = Drawing(300, canvas_height)
-        d.add(Rect(0, 30, canvas_width, 200, fillColor=colors.HexColor('#EBC9BB'), strokeColor=None))
+        d.add(Rect(0, 590, canvas_width, 200, fillColor=colors.HexColor('#EBC9BB'), strokeColor=None))
         d.add(Rect(30, 0, 210, canvas_height, fillColor=colors.HexColor('#6B9999'), strokeColor=None))
         d.drawOn(c, 0, 0)
 
@@ -65,7 +78,7 @@ def createResume(request):
         circle_drawing = Drawing(width=100, height=100)
         circle = Circle(55, 50, 90, strokeColor=None, fillColor=colors.white)
         circle_drawing.add(circle)
-        circle_drawing.drawOn(c, 80, 80)
+        circle_drawing.drawOn(c, 80, 650)
 
         #Get the image from the POST response and Place it over the circle
         if uploaded_image:
@@ -79,42 +92,57 @@ def createResume(request):
                 for chunk in uploaded_image.chunks():
                     destination.write(chunk)
 
-            #Since our canvas has the property bottomup=0, Use PIL to rotate our image
-            with PImage.open(image_path) as img:
-                img = img.transpose(PImage.FLIP_TOP_BOTTOM)
-                img.save(image_path, format='PNG')
+            # #Since our canvas has the property bottomup=0, Use PIL to rotate our image #Code Commented because bottomup=0 is no longer used
+            # with PImage.open(image_path) as img:
+            #     img = img.transpose(PImage.FLIP_TOP_BOTTOM)
+            #     img.save(image_path, format='PNG')
 
             #Place it over the circle
-            c.drawImage(image_path, 48.5, 43.5, width=173, height=173, mask='auto')
+            c.drawImage(image_path, 48.5, 613.5, width=173, height=173, mask='auto')
         
         # c.showPage()
 
         #Draw the Header text
         c.setFont('LatoBold', 28)
-        c.drawString(250, 130, fname.upper())
-        c.drawString(250, 160, lname.upper())
+        c.drawString(270, 700, fname.upper())
+        c.drawString(270, 670, lname.upper())
         c.setFont('LoraItalic', 14)
         profession_txt = """<font name="LoraItalic" size="16">Student</font>"""
         profession = Paragraph(profession_txt, style=style["Normal"])
         profession.wrapOn(c, canvas_width, canvas_height)
-        profession.drawOn(c, 250, 188, mm)
+        profession.drawOn(c, 270, 650, mm)
 
         #Draw Profile Section
         
-        profile_txt = """<font name="Lora" color="white" size="14">Business Administration student.<br/> I consider my self a responsible and orderly person.<br/> I am looking foward for my first work experience.</font>"""
-        profile = Paragraph(profile_txt, style=style["Content"])
+        contact_data = [
+            [Paragraph("""<font name="FontAwesome" color="white" size="20">&nbsp;\uf095&nbsp;</font>""", style=style['IconHere']), Paragraph(f"""<font name="Lora" color="white" size="14">&ensp;{contactNo}</font>""", style=style["Content"])],
+            [Paragraph("""<font name="FontAwesome" color="white" size="20">\uf0e0&nbsp;</font>""", style=style['IconHere']), Paragraph(f"""<font name="Lora" color="white" size="14">&ensp;{email}</font>""", style=style["Content"])],
+            [Paragraph("""<font name="FontAwesome" color="white" size="20">&nbsp;\uf041&nbsp;&nbsp;</font>""", style=style['IconHere']), Paragraph(f"""<font name="Lora" color="white" size="14">&ensp;125 Anywhere, Any City, State, Country 405475</font>""", style=style["Content"])]
+        ]
+        
+        cont_table = Table(contact_data, rowHeights=[40, 40, 60], colWidths=[35, 150])
+        cont_table.setStyle(TableStyle([
+            ('ALIGN', (0, 0), (-1, -1), "LEFT"),
+            ('VALIGN',(0,0),(-1, -1),'TOP')])
+        )
+        profile_data = [
+            [Paragraph("""<font name="LatoBold" size="16" color="white">PROFILE</font>""", style=style["SectionTitle"])], 
+            [Paragraph("""<font name="Lora" color="white" size="14">Business Administration student.<br/> I consider my self a responsible and orderly person.<br/> I am looking foward for my first work experience.</font>""", style=style["Content"])],
+            [Paragraph("""<font name="LatoBold" size="16" color="white">CONTACT ME</font>""", style=style["SectionTitle"])], 
+            [cont_table],
+            ]
+        
+        
+        
+        profile = Table(profile_data, rowHeights=[30, 250, 30, 200])
+        profile.setStyle(TableStyle([
+        ('ALIGN', (0, 0), (-1, -1), "LEFT"),
+        ('VALIGN',(0,0),(-1, -1),'TOP')])
+        )
         profile.wrapOn(c, 180, 200)
-        profile_lines = len(profile.split(180, 200))
-        profile_height = profile_lines * 14
+        profile.drawOn(c, 50, 50)
 
-        c.setFont('LatoBlack', 16)
-        c.setFillColor(colors.white)
-        c.drawString(50, 280, 'PROFILE') #Profile Header
-        profile.drawOn(c, 50, 180)
-
-        #Draw Contact Info
-        contact_me_y = 180 - profile_height - 40
-        c.drawString(50, contact_me_y, 'CONTACT ME')
+        
 
         c.save()
         if uploaded_image:
